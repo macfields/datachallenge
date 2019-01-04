66aede4c4eb8ec87f53458916169926a50881682
Maclaine Fields <maclainefields@Maclaines-MacBook-Pro.local>
2019-01-03 22:14:13 -0800

    changing from original world happiness report data to new data with correct values of gdp per capita

--- scratch.R
+++ scratch.R
@@ -5,23 +5,36 @@ library(janitor)
 #Reading in csv file froml 2017 World Happiness Report. Use clean names to
 #handle problematic variable names.
 
-y <- read_csv("2017.csv") %>% 
-  clean_names()
+z <- read_csv("world-happiness-report-data-QueryResult.csv") %>% clean_names()
+
 
 #Want to get an idea of what my dataset looks like. 
-glimpse(y)
+glimpse(z)
 
 #Univariate Analysis
-summary(y)
+summary(z)
 
-#Want to get a sense of the relationship between GDP per capita and Happiness
+#The Statistical Appendix on data.world for the World Happiness Report says that
+#there are some countries which which log_gdp_per_capita is not given. I wanted
+#to see how many observations had missing values. I then remove observations
+#with missing values.
+
+colSums(is.na(z))
+
+z <- z %>% na.omit()
+
+#Want to get a sense of the relationship between log GDP per capita and Happiness
 #score, so I use ggplot to create a scatter plot.
 
-y %>% 
-ggplot(aes(x = y$economy_gdp_per_capita, y = y$happiness_score)) + geom_point()
+z %>% 
+  ggplot(aes(x = z$log_gdp_per_capita, y = z$life_ladder)) + geom_point()
+
 
 #Creating RDS file to use in my Shiny App. I write the RDS file to a folder
 #inside of my Shiny App directory called shiny_data.
 
-write_rds(y, path = "app/shiny_data/happiness.rds")
+write_rds(z, path = "app/shiny_data/happiness.rds")
+
+
+
 
